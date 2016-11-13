95a92df1ed448942ac143d425e863fdc6d481579
MiracleLin <ycnalin@gmail.com>
2016-11-14 00:27:36 +0800

    Revert "Revert "第一次修改""
    This reverts commit 55da87510e300e97818e9baeeeb4d0e9540204ad.

--- formation_control/applications/main.c
+++ formation_control/applications/main.c
@@ -1,5 +1,6 @@
 /******************** (C) COPYRIGHT 2014 ANO Tech ********************************
   * 作者   ：匿名科创
+  *修改者  ：NJU LET_US_FLY GROUP
  * 文件名  ：main.c
  * 描述    ：主循环
  * 官网    ：www.anotc.com
