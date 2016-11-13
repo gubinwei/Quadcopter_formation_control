aa56a401332026f690120a1b1f500668342af0ca
MiracleLin <ycnalin@gmail.com>
2016-11-13 23:32:24 +0800

    Primitive code

---
+++ formation_control/build/version.d
@@ -0,0 +1,4 @@
+.\build\version.o: applications\version.c
+.\build\version.o: H:\win10_2software\Keil\ARM\ARMCC\Bin\..\include\string.h
+.\build\version.o: applications\version.h
+.\build\version.o: H:\win10_2software\Keil\ARM\ARMCC\Bin\..\include\stdint.h
